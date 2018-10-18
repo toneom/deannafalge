35c8a7a053909d5aa9c89dcd6ce956caa5f6619c
diligant <abdeluca@gmail.com>
2018-10-17 18:22:19 -0700

    mobile nav

--- assets/js/index.js
+++ assets/js/index.js
@@ -1,1 +1,11 @@
 console.log('you can use ES6 here : )')
+
+document.addEventListener("DOMContentLoaded", function(){
+
+  var mobileButton = document.getElementById("mobile-menu-button")
+
+  mobileButton.onclick = function(){
+    document.body.classList.toggle("show")
+  }
+
+});
